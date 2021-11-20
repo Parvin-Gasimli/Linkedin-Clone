825fc49e5be15970d99dfb9f397dd8ce137f98bc
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-20 23:17:29 +0300

    main Components

--- linkedin/src/Components/Main.js
+++ linkedin/src/Components/Main.js
@@ -1,15 +1,108 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
-    return (
-        <Container>
-            salam
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" />
+          <button>Start a Post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg.png" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.png" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/event-icon.png" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article.png" />
+            <span>Article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
+};
 
-export default Main
-const Container=styled.div`
-grid-area: main;
-`
+export default Main;
+const Container = styled.div`
+  grid-area: main;
+`;
+const CommonCart = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 /20%);
+`;
+const ShareBox = styled(CommonCart)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+      img {
+        width: 25px;
+        height: 25px;
+      }
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+        object-fit: cover;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        img {
+          width: 30px;
+        }
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+  }
+`;
