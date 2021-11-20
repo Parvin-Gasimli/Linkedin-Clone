6c82b7763b782ce3dfaf190d5786d4274bc727ff
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-20 20:52:55 +0300

    Rightside

--- linkedin/src/Components/LeftSide.js
+++ linkedin/src/Components/LeftSide.js
@@ -2,92 +2,199 @@ import React from "react";
 import styled from "styled-components";
 
 const LeftSide = (props) => {
-  return <Container>
+  return (
+    <Container>
       <ArtCart>
-          
-          <UserInfo>
-              <CardBackground/>
-              <a>
-                  <Photo/>
-                  <Link>Welcome,there!</Link>
-              </a>
-              <a>
-                  <AddPhotoText>
-                      Add the Photo
-                  </AddPhotoText>
-              </a>
-
-           
-          </UserInfo>
+        <UserInfo>
+          <CardBackground />
+          <a>
+            <Photo />
+            <Link>Welcome,there!</Link>
+          </a>
+          <a>
+            <AddPhotoText>Add the Photo</AddPhotoText>
+          </a>
+        </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your Network</span>
+            </div>
+            <img src="/images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="/images/item-icon.svg" />
+            My items
+          </span>
+        </Item>
       </ArtCart>
-
-
-  </Container>;
+      <CommunityCart>
+        <a>
+          <span>
+            Group
+          </span>
+        </a>
+        <a>
+          <span>
+            Events
+            <img src="/images/plus-icon.svg"/>
+          </span>
+        </a>
+        <a>
+          <span>Follow Hashtags</span>
+        </a>
+        <a>
+          <span>Discover more</span>
+        </a>
+      </CommunityCart>
+    </Container>
+  );
 };
 
 export default LeftSide;
 const Container = styled.div`
   grid-area: leftside;
 `;
-const ArtCart=styled.div`
-text-align: center;
-overflow: hidden;
-margin-bottom: 9px;
-background-color: #fff;
-border-radius: 5px;
-transition:  box-shadow 83ms;
-position: relative;
-border: none;
-box-shadow: 0 0 0 1px rgb( 0 0 0 /15%),0 0 0 rgb(0 0 0/20%);
+const ArtCart = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 9px;
+  background-color: #fff;
+  border-radius: 5px;
+  transition: box-shadow 83ms;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0/20%);
+`;
+const UserInfo = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding: 12px 12px 16px;
+  word-wrap: break-word;
+  word-break: break-word;
+`;
+const CardBackground = styled.div`
+  background: url("/images/card-bg.svg");
+  background-position: center;
+  background-size: 462px;
+  height: 54px;
+  margin: -12px -12px 0;
+`;
+const Photo = styled.div`
+  box-shadow: none;
+  background-image: url("/images/photo.svg");
+  width: 72px;
+  height: 72px;
+  box-sizing: border-box;
+  background-clip: content-box;
+  background-color: white;
+  background-size: 60%;
+  background-position: center;
+  background-repeat: no-repeat;
+  border: 2px solid white;
+  margin: -38px auto 12px;
+  border-radius: 50%;
+`;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
+const AddPhotoText = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 400;
+`;
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.9);
+  padding-top: 12px;
+  padding-bottom: 12px;
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.08);
+    }
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      span {  
+        padding:  5px 9px;
+        
+        font-size: 12px;
+        line-height: 1.333;
+        & :first-child {
+          padding:  2px 9px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+          padding: 10px 0;
+          padding:  0 9px;
+       
+          color: rgba(0, 0, 0, 0.9);
+          font-weight: bold;
+        }
+      }
+    }
+  }
+`;
+
+const Item = styled.a`
+border-color: rgba(0,0,0,0.9);
+text-align: left;
+font-size: 14px;
+font-weight: bold;
+display: block;
+span{
+  display: flex;
+  align-items: center;
+  color: rgba(0,0,0,0.6);
+}
+&:hover{
+  background-color: rgba(0,0,0,0.08);
+
+}
 
 
 
 
-`
-const UserInfo=styled.div`
-border-bottom: 1px solid rgba(0 ,0 ,0,0.15);
-padding: 12px 12px 16px;
-word-wrap: break-word;
-word-break: break-word;
+`;
+const CommunityCart=styled(ArtCart)`
+padding: 8px 0 0;
+text-align: left;
+display: flex;
+flex-direction: column;
+a{
+  color: black;
+  padding: 4px 12px 4px 12px;
+  font-size: 12px;
+  &:hover{
+    color: #0a66c2;
+  }
+  span{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-
-`
-const CardBackground=styled.div`
-background: url("/images/card-bg.svg");
-background-position: center;
-background-size: 462px;
-height: 54px;
-margin:-12px -12px 0 ;
-
-`
-const Photo=styled.div`
-box-shadow: none;
-background-image: url("/images/photo.svg");
-width: 72px ;
-height: 72px;
-box-sizing: border-box;
-background-clip: content-box;
-background-color: white;
-background-size: 60%;
-background-position: center;
-background-repeat: no-repeat;
-border: 2px solid white;
-margin:-38px auto 12px  ;
-border-radius: 50%;
-`
-const Link=styled.div`
-font-size: 16px;
-line-height: 1.5;
-color: rgba(0 ,0,0,0.9);
-font-weight: 600;
-
-`
-const AddPhotoText=styled.div`
-color: #0a66c2;
-margin-top: 4px;
-font-size: 12px;
-line-height: 1.33;
-font-weight: 400;
+  }
+  &:last-child{
+    color: rgba(0,0,0,0.6);
+    text-decoration: none;
+    border-top: 1px solid #d6cec2;
+    padding: 12px;
+    &:hover{
+      background-color: rgba(0,0,0,0.08);
+    }
+  }
+}
 
 
 `
--- linkedin/src/Components/Rightside.js
+++ linkedin/src/Components/Rightside.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Rightside = () => {
-    return (
-        <Container>
-            salam
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <FollowCart>
+          dslsm
+      </FollowCart>
+    </Container>
+  );
+};
 
-export default Rightside
-const Container=styled.div`
-grid-area: rightside;
-`
+export default Rightside;
+const Container = styled.div`
+  grid-area: rightside;
+`;
+const FollowCart = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  padding: 13px;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%) , 0 0 0 rgb( 0 0 0 /20%) ;
+`;
